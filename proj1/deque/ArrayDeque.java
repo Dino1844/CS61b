2f1e60dd2456d155da2f4f179b9b447f90f58255
Alfred <fangjiacheng2021@gmail.com>
2024-12-11 21:06:31 +0800

    modify proj1

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -3,79 +3,105 @@ package deque;
 public class ArrayDeque<Data> implements Deque<Data> {
     private Data[] a;
     private int size;
-    private int  maxSize;
+    private int maxSize;
+    private int front;
+    private int rear;
+
     public ArrayDeque() {
         a = (Data[]) new Object[8];
         size = 0;
         maxSize = 8;
+        front = 0;
+        rear = 0;
     }
-    private void resize(int len){
+
+    private void resize(int len) {
         Data[] reArray = (Data[]) new Object[len];
-        System.arraycopy(a, 0, reArray, 0, Math.min(size, len));
+        for (int i = 0; i < size; i++) {
+            reArray[i] = a[(front + i) % maxSize];
+        }
         a = reArray;
+        front = 0;
+        rear = size;
         maxSize = len;
     }
+
     @Override
-    public int size(){
+    public int size() {
         return size;
     }
+
     @Override
-    public void printDeque(){
-        for(int i = 0; i < size-1;i++){
-            System.out.print(a[i]+"->");
+    public void printDeque() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(a[(front + i) % maxSize]);
+            if (i < size - 1) {
+                System.out.print("->");
+            }
         }
-        System.out.println(a[size-1]);
+        System.out.println();
     }
+
     @Override
     public Data get(int index) {
-        return a[index];
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        return a[(front + index) % maxSize];
     }
+
     @Override
     public void addLast(Data data) {
-        if(maxSize <= size){
-            resize(2*maxSize);
+        if (size == maxSize) {
+            resize(2 * maxSize);
         }
-        a[size++] = data;
+        a[rear] = data;
+        rear = (rear + 1) % maxSize;
+        size++;
     }
+
     @Override
     public Data removeLast() {
-        Data tmp = a[--size];
-
-        if(size < maxSize/2){
-            resize(maxSize/2);
+        if (size == 0) {
+            return null;
+        }
+        rear = (rear - 1 + maxSize) % maxSize;
+        Data tmp = a[rear];
+        a[rear] = null;
+        size--;
+        if (size < maxSize / 4 && maxSize > 8) {
+            resize(maxSize / 2);
         }
         return tmp;
     }
+
     @Override
     public void addFirst(Data data) {
-        if(maxSize <= size){
-            resize(2*maxSize);
+        if (size == maxSize) {
+            resize(2 * maxSize);
         }
-        Data[] tmp = (Data[]) new Object[maxSize+1];
-        System.arraycopy(a, 0, tmp, 1, size);
-        tmp[0] = data;
-        a = tmp;
-        size ++;
+        front = (front - 1 + maxSize) % maxSize;
+        a[front] = data;
+        size++;
     }
+
     @Override
     public Data removeFirst() {
-        if(size < maxSize/2){
-            resize(maxSize/2);
+        if (size == 0) {
+            return null;
         }
-        Data tmp = a[0];
-        Data[] b = (Data[]) new Object[size-1];
-        System.arraycopy(a, 1, b, 0,size-1);
+        Data tmp = a[front];
+        a[front] = null;
+        front = (front + 1) % maxSize;
         size--;
-        a = b;
+        if (size < maxSize / 4 && maxSize > 8) {
+            resize(maxSize / 2);
+        }
         return tmp;
-
-
     }
 
-
-
     public static void main(String[] args) {
-        ArrayDeque<String> d = new ArrayDeque<String>();
+        ArrayDeque<String> d = new ArrayDeque<>();
         d.addLast("a");
         d.addLast("b");
         d.addLast("c");
